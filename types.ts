e306819e1a0694ed3d1bd467976d14f7f3382a90
Ricardo Devis Agullo <mkredz@gmail.com>
2020-04-09 19:27:29 +0200

    fix type problems

--- types.ts
+++ types.ts
@@ -15,17 +15,9 @@ interface Ingredient {
   measurement: Measurement;
 }
 
-function formatMeasurement(ingredient: Ingredient): string {
-  let measure: string;
-}
-
-function formatIngredient(ingredient: Ingredient): string {
-  return '';
-}
-
 export type Recipe = Ingredient[];
 
-const sarada: Recipe = [
+export const salad: Recipe = [
   { name: 'limón', quantity: 1 / 4, measurement: 'unidad' },
   { name: 'cilantro', quantity: 1, measurement: 'manojo' },
   { name: 'sal', quantity: 1, measurement: 'pellizco' },
