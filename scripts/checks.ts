499a2cd6b02f46794a618547d947636da1ad455e
Ricardo Devis Agullo <mkredz@gmail.com>
2020-08-30 17:47:18 +0200

    refactor generateimages

--- scripts/checks.ts
+++ scripts/checks.ts
@@ -1,32 +1,3 @@
-import { recipes } from '../recipes';
-import fs from 'fs';
-import path from 'path';
+import { generateImages } from './images';
 
-const publicPath = path.join(__dirname, '..', 'public');
-const files = fs.readdirSync(publicPath);
-
-function exit(msg: string): never {
-  console.error(msg);
-  process.exit(1);
-}
-
-for (const slug of Object.keys(recipes)) {
-  const mainPhoto = `${slug}.jpg`;
-  const mainWebpPhoto = `${slug}.webp`;
-  const smallPhoto = `${slug}-small.jpg`;
-  const smallWebpPhoto = `${slug}-small.webp`;
-
-  if (!files.includes(mainPhoto)) {
-    exit(`Not found image for ${slug}'`);
-  }
-
-  const toGenerate = [mainWebpPhoto, smallPhoto, smallWebpPhoto];
-
-  for (const photo of toGenerate) {
-    if (!files.includes(photo)) {
-      exit(`Not found image for ${photo}`);
-    }
-  }
-}
-
-console.log('All ok!');
+generateImages({ onlyCheck: true });
---
+++ scripts/images.ts
@@ -0,0 +1,45 @@
+import { recipes } from '../recipes';
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
+
+const publicPath = path.join(__dirname, '..', 'public');
+const files = fs.readdirSync(publicPath);
+
+function exit(msg: string): never {
+  console.error(msg);
+  process.exit(1);
+}
+
+export function generateImages({
+  onlyCheck = false,
+}: {
+  onlyCheck: boolean;
+}): void {
+  for (const slug of Object.keys(recipes)) {
+    const mainPhoto = `${slug}.jpg`;
+    const mainWebpPhoto = `${slug}.webp`;
+    const smallPhoto = `${slug}-small.jpg`;
+    const smallWebpPhoto = `${slug}-small.webp`;
+
+    if (!files.includes(mainPhoto)) {
+      exit(`Not found source image for ${slug}'`);
+    }
+
+    const toGenerate = [mainWebpPhoto, smallPhoto, smallWebpPhoto];
+
+    for (const photo of toGenerate) {
+      if (!files.includes(photo)) {
+        if (onlyCheck) {
+          exit(`Photo ${photo} doesn't exist`);
+        } else {
+          const image = fs.readFileSync(path.join(publicPath, mainPhoto));
+          const newImage = photo.includes('-small')
+            ? sharp(image).resize(null, 400)
+            : sharp(image);
+          newImage.toFile(path.join(publicPath, photo));
+        }
+      }
+    }
+  }
+}
