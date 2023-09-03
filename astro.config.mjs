ee39efc9b15cd32fc25819533cb2043875a51613
Ricardo Devis Agullo <mkredz@gmail.com>
2023-09-03 17:29:35 +0200

    improve imgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import dynamicImport from 'vite-plugin-dynamic-import';
 
 import vercelStatic from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  vite: { plugins: [dynamicImport()] },
   output: 'static',
   adapter: vercelStatic(),
 });
